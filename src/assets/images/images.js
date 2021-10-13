88a081f91ca0903032a0f90bc16fbfd8d12df20d
Choszcz Jakub <jakub.choszcz@gmail.com>
2021-10-13 12:49:21 +0200

    Updates

--- src/assets/images/images.js
+++
@@ -1,9 +0,0 @@
-const images = {
-    logo: require('./logo.png').default,
-    logoIcon: require('./logo-icon.png').default,
-    pikatchu1: require('./pikatchu1.png').default,
-    pikachu2: require('./pikachu2.png').default,
-    techBrands: require('./tech-brands.png').default
-}
-
-export default images;
